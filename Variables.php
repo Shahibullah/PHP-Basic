e43014af31eba7e7bd09c31aec0e000ffd9e415a
Shahib <163203412+Shahibullah@users.noreply.github.com>
2025-09-28 20:40:49 +0600

    Add files via upload

---
+++ Variables.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Class</title>
+</head>
+</html>
+<?php 
+    $isAdmin = true; 
+    $name = "AHMMED SHAHIBULLAH SHAHIB";
+    $age = 22;
+    $adress = "Dinajpur,Rangpur,Bangladesh";
+
+?>
+<html>  
+    <body>
+        <h1>Name: <?php echo $name?><br>
+        Age: <?php echo $age?><br>
+        Adress: <?php echo $adress?><br>
+
+        </h1>
+
+    </body>
+</html>

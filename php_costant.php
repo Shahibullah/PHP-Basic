31d6704c4a369d6c26a5729dfb396288afeb87ee
Shahib <163203412+Shahibullah@users.noreply.github.com>
2025-09-29 18:24:31 +0600

    Add files via upload

---
+++ php_costant.php
@@ -0,0 +1,17 @@
+<?php
+/*Ways to define costant in PHP*/
+?>
+<?php 
+define("NAME","Shahib");
+
+echo "My name is  ". NAME . "<br>";
+?>
+<?php
+/*Ways to define costant in PHP*/
+?>
+
+<?php
+const PI = 3.14159; 
+
+echo "The value of Pi: ".PI;  // Output: 3.14159
+?>

2558192af3fe7237cabb497d28dc8168766b9794
Shahib <163203412+Shahibullah@users.noreply.github.com>
2026-02-07 04:37:26 +0600

    Add files via upload

---
+++ for_loop_with_html.php
@@ -0,0 +1,12 @@
+<html>
+    <body>
+        <ol type="1">
+            <?php 
+            for ($count = 0; $count <= 50 ;$count++){
+              
+                echo "<li> User $count  </li>"; 
+            } 
+            ?>
+        </ol>
+    </body>
+</html>

77dc66b40ce0303563cc5f08d01513f12bba373c
Shahib <163203412+Shahibullah@users.noreply.github.com>
2026-02-07 18:44:22 +0600

    Refactor sum function with return type declaration
    Refactor sum function to use alternative syntax and add return type.

--- union.php
+++ union.php
@@ -1,9 +1,12 @@
 <?php 
 
-function sum(int|float $a ,int|float $b){
+function sum(int|float $a ,int|float $b):
+int | float
+{
     return $a + $b;
 
 }
 $result = sum(12.98,87);
 echo $result;
+
 ?>
